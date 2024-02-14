47fe1a38610ebb6734c26d067a365df8f5156bb0
Gr00vyGrim <116598731+Gr00vyGrim@users.noreply.github.com>
2024-02-13 16:18:52 -0800

    added all pin functionality [1-6]

--- Platform io/ESP32_Flask_encoder/ESP32_FlaskSever_DEMO/include/pin_setup.h
+++ Platform io/ESP32_Flask_encoder/ESP32_FlaskSever_DEMO/include/pin_setup.h
@@ -9,6 +9,7 @@ const int PIN_A3 = 35;
 const int PIN_A4 = 34;
 const int PIN_A5 = 18;
 const int PIN_A6 = 27;
+
 const int PIN_B1 = 26;
 const int PIN_B2 = 4;
 const int PIN_B3 = 17;
--- Platform io/ESP32_Flask_encoder/ESP32_FlaskSever_DEMO/src/pins.cpp
+++ Platform io/ESP32_Flask_encoder/ESP32_FlaskSever_DEMO/src/pins.cpp
@@ -9,18 +9,19 @@ void setUpPins()
     pinMode(PIN_I2C_SDA, OUTPUT);
 
     pinMode(PIN_LED, OUTPUT);
+    
     pinMode(PIN_A1, INPUT_PULLUP);
-    pinMode(PIN_A2, INPUT);
-    pinMode(PIN_A3, INPUT);
-    pinMode(PIN_A4, INPUT);
-    pinMode(PIN_A5, INPUT);
-    pinMode(PIN_A6, INPUT);
+    pinMode(PIN_A2, INPUT_PULLUP);
+    pinMode(PIN_A3, INPUT_PULLUP);
+    pinMode(PIN_A4, INPUT_PULLUP);
+    pinMode(PIN_A5, INPUT_PULLUP);
+    pinMode(PIN_A6, INPUT_PULLUP);
     pinMode(PIN_B1, INPUT_PULLUP);
-    pinMode(PIN_B2, INPUT);
-    pinMode(PIN_B3, INPUT);
-    pinMode(PIN_B4, INPUT);
-    pinMode(PIN_B5, INPUT);
-    pinMode(PIN_B6, INPUT);
+    pinMode(PIN_B2, INPUT_PULLUP);
+    pinMode(PIN_B3, INPUT_PULLUP);
+    pinMode(PIN_B4, INPUT_PULLUP);
+    pinMode(PIN_B5, INPUT_PULLUP);
+    pinMode(PIN_B6, INPUT_PULLUP);
 
     pinMode(PIN_PROBE, INPUT);
     pinMode(PIN_TACH, INPUT);
